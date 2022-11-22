5dc674042032311f7c81e7ebf7a898480fe01931
9thales <thalesg@protonmail.ch>
2022-11-22 11:10:38 -0300

    Alterações#3
    Menu: Um menu primitivo FUNCIONANDO
    Main: Agora a main chama menu e não o JOGO

---
+++ game/Menu.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Ente.h"
+#include <iostream>
+#include "Jogo.h"
+
+//Inicialmente nao vou derivar do ENTE e apenas seguir o TUTORIAL
+class Menu
+{
+private:
+	//Como eh derivado do ENTE jah tem o posicao
+	int pos;
+	bool pressed, theselect;
+	
+
+	//Conhece a JANELA
+	sf::RenderWindow* window;
+	sf::RectangleShape* winclose;
+
+	//A fonte
+	sf::Font* font;
+	//A textura
+	sf::Texture* image;
+	//A sprite
+	sf::Sprite* bg;
+	//Guarda as coordenadas do mouse
+	sf::Vector2i pos_mouse;
+	sf::Vector2i mouse_coord;
+
+	//As opções em tela
+	std::vector<const char*> options; //Os nomes no menu
+	std::vector<sf::Vector2f> coords;
+	//Utilizada para renderizar os textos
+	std::vector<sf::Text> texts;
+	std::vector<std::size_t> sizes;
+
+protected:
+	void set_values();
+	void loop_events();
+	void draw_all();
+
+public:
+	Menu();
+	~Menu();
+	void run_menu();
+
+	void setFont();
+};
+
